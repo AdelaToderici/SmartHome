5e4e3d7ef6d049945bafc22b039ed1fa247d8e4e
Adela Toderici <adeisa90@yahoo.com>
2016-05-11 23:10:50 +0300

    Initial Commit

---
+++ SmartHome/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SmartHome
+//
+//  Created by Adela Toderici on 5/11/16.
+//  Copyright © 2016 Adela Toderici. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SmartHome/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SmartHome
+//
+//  Created by Adela Toderici on 5/11/16.
+//  Copyright © 2016 Adela Toderici. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
